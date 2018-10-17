a3f3c02944179ac32126fd2363d95a962395bb0c
Phan Đình Minh Hiếu <16520412@gm.uit.edu.vn>
2018-10-18 01:15:36 +0700

    update

--- qualuuniemh2App/App.js
+++ qualuuniemh2App/App.js
@@ -2,25 +2,42 @@ import React from 'react';
 import { StyleSheet, Text, View , Image} from 'react-native';
 
 export default class App extends React.Component {
-  render() {
-    return (
-      <View style={styles.container}>
-		<Image
+	render() {
+		let logo = require('./assets/logo.png')
+		let background = require('./assets/hp_big_img.jpg')
+	  
+	  
+		return (
+		<View style={styles.container}>
+			<Image
 			style={{width: 50, height: 50}}
-			source={{uri: 'https://qualuuniemh2.000webhostapp.com/upload/logo.png'}}
-		/>
-	 
-        <Text>Quà lưu niệm H2</Text>
-      </View>
-    );
-  }
+			source={logo}
+			/>
+			<Text
+			style={
+				{
+					fontSize: 40,
+				}
+				
+			}
+			>Quà lưu niệm H2</Text>
+			<Image
+			style={
+				{resizeMode: 'cover'}
+			}
+			source={background}
+			/>
+		</View>
+		
+		);
+	}
 }
 
 const styles = StyleSheet.create({
-  container: {
-    
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+	container: {
+		flex: 1,
+		backgroundColor: '#fff',
+		alignItems: 'center',
+		justifyContent: 'space-around',
+	},
 });
